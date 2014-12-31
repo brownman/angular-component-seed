e6e4f438575f612cdfb8102d1b6b48f2baa31db6
brownman <brownman556@gmail.com>
2014-12-31 17:44:43 +0200

    Create filter_testing.js

---
+++ component/filter_testing.js
@@ -0,0 +1,17 @@
+myModule.filter('length', function() {
+  return function(text) {
+    return ('' + (text || '')).length;
+  }
+});
+
+describe('length filter', function() {
+  it('returns 0 when given null', function() {
+    var length = $filter('length');
+    expect(length(null)).toEqual(0);
+  });
+
+  it('returns the correct value when given a string of chars', function() {
+    var length = $filter('length');
+    expect(length('abc')).toEqual(3);
+  });
+});
